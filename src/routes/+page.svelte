e8503c7e09c848875f6ce8640515ed68b018dbc1
lucas.subli <github@lucasf.com.br>
2023-01-04 12:20:36 -0300

    removing image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,11 +11,11 @@
 	<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
 		<div class="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
 			<div class="absolute inset-0">
-				<img
+				<!-- <img
 					class="h-full w-full object-cover"
 					src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
 					alt="People working on laptops"
-				/>
+				/> -->
 				<div
 					class="absolute inset-0 bg-gradient-to-r from-fuchsia-800 to-teal-700 mix-blend-multiply"
 				/>
